ab2037731055b83f1106991a67c3b3d0000d2149
csfilipinyi <csaba.filipinyi@gmail.com>
2020-10-12 13:12:54 +0100

    oauth2

--- server/api.js
+++ server/api.js
@@ -5,8 +5,53 @@ import { Connection } from "./db";
 
 const router = new Router();
 
+const client = new AuthorizationCode({
+	client: {
+		id: process.env.CLIENT_ID,
+		secret: process.env.CLIENT_SECRET,
+	},
+	auth: {
+		tokenHost: 'https://github.com',
+		tokenPath: '/login/oauth/access_token',
+		authorizePath: '/login/oauth/authorize',
+	},
+});
+
+const authorizationUri = client.authorizeURL({
+	redirect_uri: 'https://cyf-final-project-pgsql.herokuapp.com/callback',
+	scope: 'notifications',
+	state: '3(#0/!~',
+});
+
+router.get('/login', (req, res) => {
+	console.log(authorizationUri);
+	res.redirect(authorizationUri);
+});
+
+ // Callback service parsing the authorization token and asking for the access token
+ app.get('/callback', async (req, res) => {
+    const { code } = req.query;
+    const options = {
+      code,
+    };
+
+    try {
+      const accessToken = await client.getToken(options);
+
+      console.log('The resulting token: ', accessToken.token);
+
+      return res.status(200).json(accessToken.token);
+    } catch (error) {
+      console.error('Access Token Error', error.message);
+      return res.status(500).json('Authentication failed');
+    }
+  });
+
+router.get("oauth2/", (_, res, next) => {
+	res.send('Hello<br><a href="/login">Log in with Github</a>');
+});
+
 router.get("/", (_, res, next) => {
-	
 	Connection.connect((err) => {
 		if (err) {
 			return next(err);
@@ -14,5 +59,7 @@ router.get("/", (_, res, next) => {
 		res.json({ message: "Hello, world!" });
 	});
 });
+
+
 
 export default router;
