aeae894f0f6f76dbf16f5293716f65cdc2e16fea
osagiestar <okoedooc@yahoo.com>
2020-12-08 10:53:43 +0000

    set up branch user-logout-page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Attendance Register
         </a>
       </header>
     </div>
