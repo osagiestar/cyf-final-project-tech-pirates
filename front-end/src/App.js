f2735e3abb2b3b3a9a7ccfa31b1d5cf02cf5b4ba
NihalAli2 <nihaluthman@hotmail.com>
2020-12-09 09:56:07 +0000

    Add headings

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import Heading from "./Heading";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Tech pirates
-        </a>
-      </header>
+    <div className= "container">
+    <div>
+      <Heading/>
+      </div>
+      <div>
+   <h1><span className= "font-weight-bold">Welcome!</span></h1>
+  
+   <p className= "first-line">This is an attendance register for CYF classes</p>
+    
+   </div>
+   <div>
+   <form className= "input">
+    <FormGroup>
+<div className="email">
+      <input type= "email" placeholder= "Email/User name"></input>
+
+      <input type= "Password" placeholder= "Password"></input>
+      </div>
+    </FormGroup>
+    <button className= "button">Sign in</button>
+    </form>
+    </div>
     </div>
   );
 }
---
+++ front-end/src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header>
+      <img
+        className="App-logo"
+        src="https://syllabus.codeyourfuture.io/img/logo.png"
+      />
+    </header>
+  );
+};
+
+export default Heading;
