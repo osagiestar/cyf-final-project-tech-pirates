0710f6718670974ac4e6d3b186798d7c5755fcc2
bireworld <mail2berhane@gmail.com>
2020-12-10 18:32:07 +0000

    data inserted

--- back-end/attendance.sql
+++ back-end/attendance.sql
@@ -43,4 +43,28 @@ CREATE TABLE location (
     name VARCHAR(50) NULL  NULL
 );
 
+INSERT INTO users(email,password, name, user_type,class_id) VALUES ('guy.crawford@email.com','IwasBorn1965','Guy Crawford',1,1);
+INSERT INTO users(email,password, name, user_type,class_id) VALUES ('jamal@email.com','IwasBorn1965','Jamal',2,2);
+INSERT INTO users(email,password, name, user_type,class_id) VALUES ('sara2@email.com','IwasBorn1965','Sara',3,1);
+
+INSERT INTO user_type(type) VALUES('Admin');
+INSERT INTO user_type(type) VALUES('Teacher');
+INSERT INTO user_type(type) VALUES('Student');
+
+INSERT INTO attendance(user_id,session_id) VALUES(1,5);
+INSERT INTO attendance(user_id,session_id) VALUES(2,3);
+INSERT INTO attendance(user_id,session_id) VALUES(4,2);
+
+INSERT INTO session(name,session_date,class_id) VALUES('JS','12/08/2020',2);
+INSERT INTO session(name,session_date,class_id) VALUES('React','06/09/2020',3);
+INSERT INTO session(name,session_date,class_id) VALUES('SQL','25/11/2020',2);
+
+INSERT INTO class(name,location_id) VALUES('WestMidland',2);
+INSERT INTO class(name,location_id) VALUES('London',1);
+INSERT INTO class(name,location_id) VALUES('Manchester',3);
+
+
+INSERT INTO location(name) VALUES('Manchester');
+INSERT INTO location(name) VALUES('London');
+INSERT INTO location(name) VALUES('WestMidland');
 
