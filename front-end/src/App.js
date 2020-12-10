a8b69a2e798276ce6d5b32441867a503f44bfbf9
osagiestar <okoedooc@yahoo.com>
2020-12-10 01:06:31 +0000

    created the components file, UserLogoutPage component and rendered it on the App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,36 @@
-
-import './App.css';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import "./App.css";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import Heading from "./Heading";
+import UserLogoutPage from "./components/UserLogoutPage";
 
 function App() {
   return (
-    <div className= "container">
-    <div>
-      <Heading/>
+    <div className="container">
+      <div>
+        <Heading />
       </div>
       <div>
-   <h1><span className= "font-weight-bold">Welcome!</span></h1>
-  
-   <p className= "first-line">This is an attendance register for CYF classes</p>
-    
-   </div>
-   <div>
-   <form className= "input">
-    <FormGroup>
-<div className="email">
-      <input type= "email" placeholder= "Email/User name"></input>
+        <h1>
+          <span className="font-weight-bold">Welcome!</span>
+        </h1>
 
-      <input type= "Password" placeholder= "Password"></input>
+        <p className="first-line">
+          This is an attendance register for CYF classes
+        </p>
       </div>
-    </FormGroup>
-    <button className= "button">Sign in</button>
-    </form>
-    </div>
+      <div>
+        <form className="input">
+          <FormGroup>
+            <div className="email">
+              <input type="email" placeholder="Email/User name"></input>
+
+              <input type="Password" placeholder="Password"></input>
+            </div>
+          </FormGroup>
+          <button className="button">Sign in</button>
+        </form>
+      </div>
+      <UserLogoutPage />
     </div>
   );
 }
