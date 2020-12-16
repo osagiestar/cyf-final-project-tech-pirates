820faa40e51785c7349014c5a17f29efb1ac6c8b
NihalAli2 <nihaluthman@hotmail.com>
2020-12-16 19:33:50 +0000

    add componant with silict button

---
+++ front-end/src/components/UserCanSelectClass.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { FaRegUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const UserCanSelectClass = ({onLogout}) => {
+
+        return (
+            <div className="App">
+              <h2>Please choose the class that you want to log into from the list below</h2>
+              <p>choose a class:</p>
+              <div>
+                  <select>
+                      <option>
+                         React
+                      </option>
+                      <option>
+                          javascript
+                      </option>
+                      <option>
+                          node
+                      </option>
+                      <option>
+                          SQL
+                      </option>
+                  </select>
+              </div>
+            </div>
+          );
+        
+    
+}
+export default UserCanSelectClass
