0e27ecc3bd9e486ed756c4678b4ba44ee4a46957
NihalAli2 <nihaluthman@hotmail.com>
2020-12-17 18:39:23 +0000

    change PORT

--- back-end/server.js
+++ back-end/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
