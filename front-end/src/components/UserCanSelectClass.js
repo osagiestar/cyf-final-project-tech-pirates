3580c8d6bce6be8d6286d68acc088510e7ebaa8e
bireworld <mail2berhane@gmail.com>
2020-12-18 20:08:51 +0000

    front end session login completed

--- front-end/src/components/UserCanSelectClass.js
+++ front-end/src/components/UserCanSelectClass.js
@@ -1,39 +1,49 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { FaRegUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 
 
-const UserCanSelectClass = ({onLogout}) => {
-    const [class,setClass] =useState("");
+const UserCanSelectClass = ({onLogout,id, setSessions}) => {
+     const [session,setSession] = useState([]);
+     const [sessionSelect,setSessionSelect] = useState(-1);
+     const [clicked,setClicked] = useState(false)
+console.log(sessionSelect)
+     const onclick=()=>{
+         fetch(`http://localhost:3000/users/${id}/class/session`, {
+           method: "POST",
+           body: JSON.stringify({
+             sessionId: sessionSelect
+           }),
+           headers: { "Content-Type": "application/json" },
+         }).then((res) => setClicked(true))
+     }
 
-    const classSelect = ()=>{
-        fetch('http://localhost:3000/users/:studentId/class/session', {
-            method:"GET",
-        headers:{"content-Type": "application-Json"},
-    })
-    .then((res)=>res.json(
-       
-    ))
-    .then((data)=> {classSelect(data)});
-    
-        return (
+
+     useEffect(()=>{
+        fetch(`http://localhost:3000/users/${id}/class/session`)
+    .then((res)=>res.json())
+    .then((data)=> {setSession(data)});
+    },[id])
+    console.log(session)
+        return (<>
             <div className="App">
                
               <h2>Please choose the class that you want to log into from the list below</h2>
               <p>choose a class:</p>
               <div>
-                  <select>
-                      <option value = {class}
-                      onChange = {setClass}
-                      >
-                      </option>
+                  <select value={sessionSelect} onChange={(event)=>setSessionSelect(event.target.value)} disabled={clicked} >
+                      <option value = {-1} disabled>Please select session</option>
+                      {session.map((item)=><option value = {item.id}>{item.name}</option>)}
                   </select>
+                  
               </div>
               <div className="Space">
-              <button className="Star">Log into this class</button>
+              <button className={clicked?"Star-clicked":"Star"} onClick={onclick} >{clicked?"Attending":"Log into this class"}</button>
               </div>
             </div>
+            </>
           )
+          
 }
-export default UserCanSelectClass
+export default UserCanSelectClass;
--- front-end/src/components/Welcome.js
+++ front-end/src/components/Welcome.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaRegUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Class_select from "./Class_select";
+import UserCanSelectClass from "./UserCanSelectClass"
 
-const Welcome = ({onLogout}) => {
+const Welcome = ({ userId }) => {
+  const [sessions, setSessions] = useState(false);
+    function sessionHandler(e) {
+    // setSessions(!sessions);
+    // let editId = e.target.parentNode.parentNode.id;
+    // setStudentId(editId);
+  }
   return (
     <div className="App">
       <h2>Welcome to the class</h2>
       <p>You are now logged in as a Student </p>
       <button className="Star"> More ahead </button>
-      <Class_select />
+      {/* <Class_select /> */}
+      <UserCanSelectClass id={userId} setSessions={setSessions} />
     </div>
   );
-}
+};
 
 export default Welcome;
