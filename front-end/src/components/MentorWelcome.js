bb57a83415f9a193ba9cacd52540c3c44729fa6b
bireworld <mail2berhane@gmail.com>
2020-12-25 21:41:47 +0000

    fornt-end update

--- front-end/src/components/MentorWelcome.js
+++ front-end/src/components/MentorWelcome.js
@@ -4,15 +4,18 @@ import React from "react";
 const MentorWelcome = ({ onCheckStudentClick,onCheckSessionClick }) => {
   return (
     <div className="App">
-      <h2>Welcome to the class</h2>
-      <p>You are now logged in as a Mentor </p>
-      <button className="button" onClick={onCheckStudentClick}>
-        Check students
-      </button>
-      <button className="button" onClick={onCheckSessionClick}>
-        {" "}
-        Check sessions
-      </button>
+      <div>
+        <h2>Welcome to the class</h2>
+      </div>
+      <div>
+        <p>You are now logged in as a Mentor </p>
+      </div>
+      <div className="mentor-button">
+        <button onClick={onCheckStudentClick}>Check students</button>
+      </div>
+      <div className="mentor-button">
+        <button onClick={onCheckSessionClick}> Check sessions</button>
+      </div>
     </div>
   );
 };
