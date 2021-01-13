ce395a121ef01ee74b636c93db99d330a75fa3ac
osagiestar <okoedooc@yahoo.com>
2021-01-13 16:17:28 +0000

    styled the mentor session-list, moved the backToWelcome button to the left

--- front-end/src/components/SessionList.js
+++ front-end/src/components/SessionList.js
@@ -13,21 +13,21 @@ const SessionList = ({ classId, onGoBackClick, onSessionClick }) => {
   },[classId]);
   console.log(sessionList);
   return (
-    <div className="student-list">
-      <div>
+    <div className="session-list">
+      <div className="back-to-welcome">
         <button className="button" onClick={onGoBackClick}>
           BackToWelcome
         </button>
       </div>
-      <div>
-        <h3>Session List</h3>
+      <div className="list">
+        <h3 className="add-color">Session List</h3>
       </div>
-      <div>
+      <div className="list">
         {sessionList.map((item) => (
           <Table className="table">
             <tr onClick={() => onSessionClick(item.id)}>
               {/* <td>{item.id}</td> */}
-              <td>{item.name}</td>
+              <td className="session-style">{item.name}</td>
               <td>
                 {" "}
                 <FcRight />
