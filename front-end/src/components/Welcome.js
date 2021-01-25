4ac8d744b97b0945325c55787bbb5ccb5f6d4151
osagiestar <okoedooc@yahoo.com>
2021-01-25 20:47:36 +0000

    styling student login page

--- front-end/src/components/Welcome.js
+++ front-end/src/components/Welcome.js
@@ -4,13 +4,12 @@ import UserCanSelectClass from "./UserCanSelectClass"
 
 const Welcome = ({ userId }) => {
   return (
-     return (
     <div>
       <div>
         <h2 className="welcome-header">Welcome to the class</h2>
       </div>
       <div>
-        <p className="welcome-header">You are now logged in as a Student </p>
+        <p className="login-header">You are now logged in as a Student </p>
       </div>
       <div>
         <UserCanSelectClass id={userId}/>
