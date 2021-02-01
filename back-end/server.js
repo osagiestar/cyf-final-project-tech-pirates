ce07f3d8f98570b78795235f15ddd1fbc3465b61
bireworld <mail2berhane@gmail.com>
2021-02-01 18:29:13 +0000

    backend update

--- back-end/server.js
+++ back-end/server.js
@@ -11,17 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const pool = new Pool({
-  // connectionString: process.env.DATABASE_URL,
-  // sslmode: require,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
-  user: "S225693",
+  connectionString: process.env.DATABASE_URL,
+  sslmode: require,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // user: "S225693",
 
-  host: "localhost",
-  database: "attendance",
-  password: process.env.DB_PASSWORD,
-  port: 5432,
+  // host: "localhost",
+  // database: "attendance",
+  // password: process.env.DB_PASSWORD,
+  // port: 5432,
 });
 
 /* All Users Login API */
